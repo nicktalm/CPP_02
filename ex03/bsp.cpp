2356e683ecbc5063ded7f476d6b22dfeb16ffb4a
Nick Talmon <ntalmon@2-D-6.42heilbronn.de>
2024-08-07 11:30:12 +0200

    working on ex03

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: ntalmon <ntalmon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 14:40:23 by ntalmon           #+#    #+#             */
-/*   Updated: 2024/08/06 15:37:43 by ntalmon          ###   ########.fr       */
+/*   Updated: 2024/08/06 16:58:40 by ntalmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,6 @@ float	surface(Point a, Point b, Point c)
 
 	surface = 0.5 * (a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY()));
 	if (surface < 0)
-		return surface * -1;
-	return surface;
+		return (surface * -1);
+	return (surface);
 }
